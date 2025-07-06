5026d2c80076ed14311b0e59349cb2c12c7bf0ae
Elias Adjetey <elias.adjetey@amalitechtraining.org>
2025-07-06 20:07:40 +0000

    docs: add Swagger OpenAPI configuration for Order Service

---
+++ order-service/src/main/java/com/bytebites/order_service/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.bytebites.order_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for setting up Swagger OpenAPI documentation.
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info().title("ByteBites Order Service API").version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
