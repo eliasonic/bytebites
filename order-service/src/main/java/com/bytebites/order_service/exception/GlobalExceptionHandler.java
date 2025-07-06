f64e9b1018e96852e9356663c330e75b967010c1
Elias Adjetey <elias.adjetey@amalitechtraining.org>
2025-07-06 20:08:35 +0000

    feat: add global exception handling to Order Service with consistent error responses

---
+++ order-service/src/main/java/com/bytebites/order_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.bytebites.order_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+/**
+ * Global exception handler for handling exceptions from all controllers.
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException e) {
+        ErrorResponse errorResponse = buildErrorResponse(e, null,"FORBIDDEN");
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthError(AuthenticationException e) {
+        ErrorResponse errorResponse = buildErrorResponse(e, null,"UNAUTHORIZED");
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorResponse errorResponse = buildErrorResponse(ex, request, "RESOURCE_NOT_FOUND");
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse errorResponse = buildErrorResponse(ex, request, "INTERNAL_SERVER_ERROR");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ErrorResponse buildErrorResponse(Exception ex, WebRequest request, String errorCode) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request != null ? request.getDescription(false) : null,
+                errorCode);
+    }
+}
