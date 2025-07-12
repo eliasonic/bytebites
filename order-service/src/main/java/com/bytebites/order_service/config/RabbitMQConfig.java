e9e7a0c2bd641bc351a73210ea0f5d2e4afb9053
Elias Adjetey <elias.adjetey@amalitechtraining.org>
2025-07-12 00:41:48 +0000

    chore: externalize RabbitMQ exchange and binding key configurations

--- order-service/src/main/java/com/bytebites/order_service/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/bytebites/order_service/config/RabbitMQConfig.java
@@ -1,20 +1,25 @@
 package com.bytebites.order_service.config;
 
+import lombok.Data;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "spring.rabbitmq.queue")
 public class RabbitMQConfig {
-    public static final String EXCHANGE = "order.events";
-    public static final String ROUTING_KEY = "order.placed";
+    private String exchange;
+    private String bindingKey;
 
     @Bean
     public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
+        return new TopicExchange(exchange);
     }
 
     @Bean
